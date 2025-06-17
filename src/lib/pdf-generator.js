0e785d6ad1814b5885b1170481328618d1648eb9
hain <hainofficialweb@gmail.com>
2025-06-17 11:40:02 +0530

    update PDF generator

--- src/lib/pdf-generator.js
+++ src/lib/pdf-generator.js
@@ -1,4 +1,4 @@
-import { Document, Page, Text, View, StyleSheet, PDFDownloadLink, pdf } from '@react-pdf/renderer';
+import { Document, Page, Text, View, StyleSheet, pdf } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
   page: {
